478a4b944f42c3b1dfd8c79cae18ba27c22e11d8
Rohin Galhotra <jiminycracken@gmail.com>
2018-10-25 16:05:05 -0700

    updated spawn script

--- Kaiju Defense Simulator 3D/Assets/Scripts/spawnObject.cs
+++ Kaiju Defense Simulator 3D/Assets/Scripts/spawnObject.cs
@@ -15,43 +15,46 @@ public class spawnObject : MonoBehaviour {
     public Text resText;
     // Use this for initialization
     void Start () {
-        // int width = 800; // or something else
-        // int height = 900; // or something else
-        // bool isFullScreen = false; // should be windowed to run in arbitrary resolution
-        // int desiredFPS = 60; // or something else
+        int width = 1920; // or something else
+        int height = 1080; // or something else
+        bool isFullScreen = true; // should be windowed to run in arbitrary resolution
+        int desiredFPS = 60; // or something else
 
-        // Screen.SetResolution(width, height, isFullScreen, desiredFPS);
+        Screen.SetResolution(width, height, isFullScreen, desiredFPS);
     }
 	
 	// Update is called once per frame
 	void Update () {
         resText.text = resources.ToString();
+        RaycastHit hit;
         if (Input.GetMouseButtonDown(0)) {
-            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            spawnPosition.z = 0.0f;
-            if (spawnPosition.y < -3.5 || spawnPosition.y > 4.5) return;
-            switch (type) {
-                case 0:
-                    //do nothing
-                    break;
-                case 1:
-                    if(resources >= 50) {
-                        GameObject inf = Instantiate(infantry, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
-                        resources -= 50;
-                    }
-                    break;
-                case 2:
-                    if(resources >= 100) {
-                        GameObject tan = Instantiate(tank, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
-                        resources -= 100;
-                    }
-                    break;
-                case 3:
-                    if(resources >= 200) {
-                        GameObject art = Instantiate(artillery, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
-                        resources -= 200;
-                    }
-                    break;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+            if(Physics.Raycast(ray, out hit)) {
+                Vector3 spawnPosition = hit.point;
+                if (spawnPosition.x < 0.0f || spawnPosition.x > 500.0f || spawnPosition.z > 500.0f || spawnPosition.z < 0.0f || spawnPosition.y < 0.0f || spawnPosition.y > 1.0f) return;
+                switch (type) {
+                    case 0:
+                        //do nothing
+                        break;
+                    case 1:
+                        if (resources >= 50) {
+                            GameObject inf = Instantiate(infantry, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
+                            resources -= 50;
+                        }
+                        break;
+                    case 2:
+                        if (resources >= 100) {
+                            GameObject tan = Instantiate(tank, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
+                            resources -= 100;
+                        }
+                        break;
+                    case 3:
+                        if (resources >= 200) {
+                            GameObject art = Instantiate(artillery, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
+                            resources -= 200;
+                        }
+                        break;
+                }
             }
         }
     }
