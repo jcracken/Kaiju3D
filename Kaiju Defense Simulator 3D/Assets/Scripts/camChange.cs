2ac5815e789ca2879b5d0594dc5defad1ebc4484
Rohin Galhotra <jiminycracken@gmail.com>
2018-10-28 09:58:44 -0700

    changed camera setup added ui

---
+++ Kaiju Defense Simulator 3D/Assets/Scripts/camChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(224.0f, 579.0f, 215.0f);
+        transform.rotation = Quaternion.Euler(90.0f, -90.0f, 0.0f);
+	}
+
+    // Update is called once per frame
+    public void change() {
+        transform.position = new Vector3(513.0f, 161.0f, 190.0f);
+        transform.rotation = Quaternion.Euler(28.537f, -70.388f, -1.293f);
+    }
+}
